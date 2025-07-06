26db07be5cb6997fb41c432c7aee3a1f74e1f6c9
VootXMusic <updatebot59@gmail.com>
2025-07-06 10:58:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -89,21 +89,21 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/09o5o7.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/fy7eih.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/09o5o7.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/fy7eih.jpg"
 )
-PLAYLIST_IMG_URL = "https://files.catbox.moe/09o5o7.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/09o5o7.jpg"
-TELEGRAM_AUDIO_URL = "https://files.catbox.moe/09o5o7.jpg"
-TELEGRAM_VIDEO_URL = "https://files.catbox.moe/09o5o7.jpg"
-STREAM_IMG_URL = "https://files.catbox.moe/09o5o7.jpg"
-SOUNCLOUD_IMG_URL = "https://files.catbox.moe/09o5o7.jpg"
-YOUTUBE_IMG_URL = "https://files.catbox.moe/09o5o7.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/09o5o7.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/09o5o7.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/09o5o7.jpg"
+PLAYLIST_IMG_URL = "https://files.catbox.moe/fy7eih.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/fy7eih.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/fy7eih.jpg"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/fy7eih.jpg"
+STREAM_IMG_URL = "https://files.catbox.moe/fy7eih.jpg"
+SOUNCLOUD_IMG_URL = "https://files.catbox.moe/fy7eih.jpg"
+YOUTUBE_IMG_URL = "https://files.catbox.moe/fy7eih.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/fy7eih.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/fy7eih.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/fy7eih.jpg"
 
 
 def time_to_seconds(time):
