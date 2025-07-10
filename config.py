2395adfff86b7362ac825a775a06d2d4fb9287d4
VootXMusic <updatebot59@gmail.com>
2025-07-10 22:47:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/wynkupdate143/VootXMusic",
+    "https://github.com/VOOTTV143/AMAZON",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
